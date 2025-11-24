37a157cc1c97d1984c489cbd17f1f6b151f47bf5
adil <adilrysbek0v77@gmail.com>
2025-11-24 20:02:38 +0600

    7-hm

--- homework/homework-7.py
+++ homework/homework-7.py
@@ -10,27 +10,28 @@ def create_table(conn):
         publication_year INTEGER,
         genre TEXT,
         number_of_pages INTEGER,
-        number_of_pages_of_author INTEGER
+        number_of_copies INTEGER
     )
     ''')
-def insert_book(conn, name,author,publication_year,genre,number_of_pages,number_of_pages_of_author):
+def insert_book(conn, name,author,publication_year,genre,number_of_pages,number_of_copies):
     conn.execute('''
-    INSERT INTO books(name_book  , author, publication_year, genre, number_of_pages, number_of_pages_of_author)
+    INSERT INTO books(name_book , author, publication_year, genre, number_of_pages, number_of_copies)
     VALUES (?, ?, ?, ?, ?, ?)
     ''',
-    (name, author, publication_year, genre, number_of_pages, number_of_pages_of_author)
+    (name, author, publication_year, genre, number_of_pages, number_of_copies)
     )
     conn.commit()
 if __name__ == '__main__':
     conn = sqlite3.connect('books.db')
     create_table(conn)
 
-    for i in range(int(input('Введите сколько книг вы хотите добавить в базу(минимум 10 книг): '))):
-        insert_book(conn,
-                    input('Enter name: '),
-                    input('Enter author: '),
-                    int(input('Enter publication year: ')),
-                    input('Enter genre: '),
-                    int(input('Enter number of pages: ')),
-                    int(input('Enter number of copies: '))
-                    )
+    insert_book(conn, 'Война и мир', 'Лев Толстой', 1869, 'Роман', 1225, 10)
+    insert_book(conn, 'Преступление и наказание', 'Фёдор Достоевский', 1866, 'Роман', 672, 7)
+    insert_book(conn, 'Мастер и Маргарита', 'Михаил Булгаков', 1967, 'Роман', 480, 12)
+    insert_book(conn, '1984', 'Джордж Оруэлл', 1949, 'Антиутопия', 328, 15)
+    insert_book(conn, 'Гарри Поттер и философский камень', 'Дж. Ролинг', 1997, 'Фэнтези', 432, 20)
+    insert_book(conn, 'Маленький принц', 'Антуан де Сент-Экзюпери', 1943, 'Сказка', 96, 18)
+    insert_book(conn, 'Отцы и дети', 'Иван Тургенев', 1862, 'Роман', 290, 6)
+    insert_book(conn, 'Собака Баскервилей', 'Артур Конан Дойл', 1902, 'Детектив', 256, 9)
+    insert_book(conn, 'Мёртвые души', 'Николай Гоголь', 1842, 'Роман', 352, 8)
+    insert_book(conn, 'Алхимик', 'Пауло Коэльо', 1988, 'Притча', 208, 14)
